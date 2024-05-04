df5ad4cb13f3a83897d7d9728c7f9f92d5d79cff
Script-Demon <tanveermahmud0@gmail.com>
2024-05-04 10:55:28 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// Ans to the question number (1)
+
+    // <!DOCTYPE html>
+    // <html>
+    // <head>
+    //     <title>JavaScript HTML Connection</title>
+    // </head>
+    // <body>
+
+    // <h1>Hello, World!</h1>
+
+    // <script src="script.js"></script>
+
+    // </body>
+    // </html>
+
+// Ans to the question number (2)
+
+    // here are three examples of how we can write variables in JavaScript:
+
+    // 1.Using the var keyword:
+    // var age = 30;
+    // var name = "Tanvir";
+
+    // 2.Using the let keyword:
+    // let temperature = 25.5;
+    // let city = "Dhaka";
+
+    // 3.Using the const keyword:
+    // const PI = 3.14159;
+    // const COMPANY_NAME = "Coderstrust Bangladesh";
+
+// Ans to the question number (3)
+
+    // var: var is functional, elevated, and allows reassignment.
+    // let: let is block-specific, elevated, and allows reallocation.
+    // const: const is block-wide, non-elevated, and does not allow redefinition, although it does allow mutation of objects and arrays.
+
+// Ans to the question number (4)
+
+    // console.log("Tanvir Mahmud");
+
+// Ans to the question number (5)
+
+    // alert("Tanvir Mahmud");
+
+// Ans to the question number (6)  
+
+    // var firstNumber = 10;
+    // var secondNumber = 20;
+
+    // console.log(firstNumber + secondNumber);
